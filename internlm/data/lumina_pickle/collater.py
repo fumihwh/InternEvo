e61d77931408c501308a7639c4d8e2fc582c8c9c
hujiakai <zxc200855909@qq.com>
2024-11-11 14:43:07 +0800

    dataset 对齐 seqlen

--- internlm/data/lumina_pickle/collater.py
+++ internlm/data/lumina_pickle/collater.py
@@ -1,56 +1,32 @@
 import torch
 
-def extend_data_to_packed_length(data: list, max_length, padding):
-    """Helper function to extend data to the specified length with padding."""
-    if len(data) > max_length:
-        return data[:max_length]
-    if len(data) == max_length:
-        return data
-    # len(data) < max_length
-    return data + [padding] * (max_length - len(data))
-
 def lumina_collate_fn(batch):
     """
     Collate function for packed input sequences.
-
     Args:
         batch (List[Dict]): List of dictionaries representing each sample in batch.
             Each dictionary contains "tokens", "labels", "type_ids", "cu_seqlens", and "indexes" keys.
-
     Returns:
         Tuple[Dict[str, torch.Tensor], torch.Tensor]: A tuple containing a dictionary of tensors with "input_ids",
             "cu_seqlens", "indexes", and "type_ids" keys, and the tensor of padded "labels".
     """
     # Initialize lists to store the data from each sample
     tokens, labels, type_ids, indexes = [], [], [], []
-
-    # Calculate the maximum sequence length
-    max_length = max(len(sample["tokens"]) for sample in batch)
-    max_length = min(max_length, batch[0]["seq_len"])
-
-    cumulative_seqlens = [0, max_length]
+    cumulative_seqlens = [0]
 
     # Accumulate all samples into respective lists
     for sample in batch:
-        tokens.extend(extend_data_to_packed_length(sample["tokens"], max_length, 0.0))
-        labels.extend(extend_data_to_packed_length(sample["labels"], max_length, -100.0))
-        type_ids.extend(extend_data_to_packed_length([], max_length, 0.0))
-        indexes.extend(list(range(max_length)))
-        cumulative_seqlens.append(cumulative_seqlens[-1] + max_length)
+        tokens.extend([abs(w) for w in sample["tokens"]])
+        labels.extend([w if w > 0 else -100 for w in sample["labels"]])
+        type_ids.extend(sample["type_ids"])
+        indexes.extend(sample["indexes"])
+        cumulative_seqlens.append(cumulative_seqlens[-1] + sample["cu_seqlens"][-1])
 
-    # Convert lists to tensors
+    # Convert lists to tensors and unsqueeze to add batch dimension
     xs = torch.tensor(tokens, dtype=torch.long).unsqueeze(0)
     ys = torch.tensor(labels, dtype=torch.long).unsqueeze(0)
     ts = torch.tensor(type_ids, dtype=torch.long).unsqueeze(0)
     indexes = torch.tensor(indexes, dtype=torch.long).unsqueeze(0)
     cu_seqlens = torch.tensor(cumulative_seqlens, dtype=torch.int).unsqueeze(0)
 
-    # Create the output dictionary
-    input_data = {
-        "input_ids": xs,
-        "cu_seqlens": cu_seqlens,
-        "indexes": indexes,
-        "type_ids": ts
-    }
-
-    return input_data, ys
+    return {"input_ids": xs, "cu_seqlens": cu_seqlens, "indexes": indexes, "type_ids": ts}, ys
